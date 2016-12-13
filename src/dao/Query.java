b74ce492bac2b5227e55b0343a7cbcbacd5df1f1
BlackLee_home <webitlee@163.com>
2016-12-13 23:26:51 +0800

    书写了dao包中的Query类的部分逻辑

--- src/dao/Query.java
+++ src/dao/Query.java
@@ -1,5 +1,23 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Query {
-   
+   public List<Connection> Query(String dbName, int id){
+	   List<Connection> data = new ArrayList();
+	   Connection conn = util.HandleConnection.getConnection();
+	   String sql = "select * from ? where id = ?";
+	   PreparedStatement ps = conn.prepareStatement(sql);
+	   ps.setString(1, dbName);
+	   ps.setInt(2, id);
+	   ResultSet rs = ps.executeQuery();
+	   while(rs.next()){
+		   data.add(rs.getRow());
+	   }
+	   return data;
+   }
 }
