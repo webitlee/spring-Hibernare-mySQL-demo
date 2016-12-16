96e3979ff27b0be2e32cf65f5b1c722b9b9e67d8
Black Lee <webitlee@163.com>
2016-12-16 17:13:43 +0800

    使用jsp重构了逻辑

--- src/dao/Query.java
+++ src/dao/Query.java
@@ -3,16 +3,18 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import entity.Employees;
+import util.HandleConnection;
 
 public class Query {
 	@SuppressWarnings("finally")
 	public static List<Employees> findAll(String tableName){
 		List<Employees> data = new ArrayList<Employees>();
-		Connection conn = util.HandleConnection.getConnection();
+		Connection conn = HandleConnection.getConnection();
 		String sql = "select * from " + tableName;
 		PreparedStatement ps;
 		try {
@@ -30,8 +32,30 @@ public class Query {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally{
-			util.HandleConnection.closeConnection();
+			HandleConnection.closeConnection();
 			return data;
+		}
+	}
+	@SuppressWarnings("finally")
+	public static Employees findById(String tableName, int id){
+		Employees emp = new Employees();
+		try {
+			Connection conn = HandleConnection.getConnection();
+			String sql = "select * from " + tableName + " where id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				emp.setId(id);
+				emp.setName(rs.getString("name"));
+				emp.setSalary(rs.getFloat("salary"));
+				emp.setAge(rs.getInt("age"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			HandleConnection.closeConnection();
+			return emp;
 		}
 	}
 //	public static void main(String[] args){
