0238ddaadbc960045fc79938531d3ac4263e92c3
Black Lee <webitlee@163.com>
2017-01-07 11:13:40 +0800

    补充添加jstl的标签处理类

---
+++ src/web/JstlServlet.java
@@ -0,0 +1,48 @@
+package web;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.Query;
+import entity.Employees;
+
+/**
+ * Servlet implementation class JstlServlet
+ */
+@WebServlet("/jstl")
+public class JstlServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JstlServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Employees emp = Query.findById("employee", 1);
+		request.setAttribute("emp", emp);
+		request.getRequestDispatcher("/jsp/jstl.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
