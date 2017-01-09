05dd1462454105d0e391f9ac063bf7af23d23e22
Black Lee <webitlee@163.com>
2017-01-09 15:51:09 +0800

    修改案例

--- src/diyTag/FmtDate.java
+++ src/diyTag/FmtDate.java
@@ -12,7 +12,6 @@ public class FmtDate extends SimpleTagSupport{
 
 	@Override
 	public void doTag() throws JspException, IOException {
-		super.doTag();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss e");
 		String date = sdf.format(new Date());
 		JspWriter jw = getJspContext().getOut();
